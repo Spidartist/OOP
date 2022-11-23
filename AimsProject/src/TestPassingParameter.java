e9f249c56d6c9c332a9071ce29df7541ca2dd844
hoang.danh.quan <hoang.danh.quan@sun-asterisk.com>
2022-11-23 21:38:46 +0700

    correct swap

--- AimsProject/src/TestPassingParameter.java
+++ AimsProject/src/TestPassingParameter.java
@@ -13,10 +13,11 @@ public class TestPassingParameter {
 		System.out.println("Jungle dvd title: " + jungleDVD.getTitle());
 	}
 	
-	public static void swap(Object o1, Object o2) {
-		Object tmp = o1;
-		o1 = o2;
-		o2 = tmp;
+	public static void swap(DigitalVideoDisc o1, DigitalVideoDisc o2) {
+		String title1 = o1.getTitle();
+		String title2 = o2.getTitle();
+		o1.setTitle(title2);
+		o2.setTitle(title1);
 	}
 	
 	public static void changeTitle(DigitalVideoDisc dvd, String title) {
